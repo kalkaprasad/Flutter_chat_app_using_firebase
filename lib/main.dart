7be5c740bb4e2f553eab1cd0cc61325ae69b6369
kalkaprasad <52508495+kalkaprasad@users.noreply.github.com>
2020-04-25 00:08:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flash_chat_firebasechat/screens/welcome_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
